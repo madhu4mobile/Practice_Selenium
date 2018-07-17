8edd8c45525c2e1d0ba506df44eb50d41a6fb83c
Madhu Muppala <madhu4target@gmail.com>
2018-07-16 22:49:25 -0400

    Commit from Ausus - Defective?

---
+++ JuneSessionStart/src/main/java/day04DropDown.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day04DropDown {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\selDrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get(arg0);
+
+	}
+
+}
